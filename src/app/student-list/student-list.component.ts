0924829639b0fd8de67317f316e3605e2c95b1c7
Gayatri Pisya <gayatripisya@gmail.com>
2024-10-18 10:59:20 +0530

    updated

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Student {
+  rollNumber: number;
+  name: string;
+  age: number;
+  course: string;
+}
 
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
-  styleUrl: './student-list.component.css'
+  styleUrls: ['./student-list.component.css']
 })
 export class StudentListComponent {
-
+  students: Student[] = [
+    { rollNumber: 146, name: 'Sireesha', age: 20, course: 'ST' },
+    { rollNumber: 149, name: 'Gayatri', age: 20, course: 'ST' },
+    { rollNumber: 155, name: 'Kishore', age: 20, course: 'ST' }
+  ];
 }
