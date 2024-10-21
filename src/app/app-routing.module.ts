e46e08c0f97a81029e4ef62f2f6a76e3ad78335a
Gayatri Pisya <gayatripisya@gmail.com>
2024-10-21 15:16:46 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { LoginButtonFunctionComponent } from './login-button-function/login-button-function.component';
-// import { NumberCheckComponent } from './number-check/number-check.component';
-// import { StudentComponent } from './student-details/student-details.component';
+import { LoginButtonFunctionComponent } from './login-button-function/login-button-function.component';
+import { NumberCheckComponent } from './number-check/number-check.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
 
 const routes: Routes = [
-  // { path: 'login', component: LoginButtonFunctionComponent },
-  // { path: 'number', component: NumberCheckComponent },
-  // { path: 'student', component: StudentComponent },
+  { path: 'login', component: LoginButtonFunctionComponent },
+  { path: 'number', component: NumberCheckComponent },
+  { path: 'student', component: StudentDetailsComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' } 
 ];
 
