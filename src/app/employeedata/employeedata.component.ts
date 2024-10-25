ca8f894f705834691ca72eaf46701fb5e7ce1c56
Gayatri Pisya <gayatripisya@gmail.com>
2024-10-25 10:09:27 +0530

    updated

--- src/app/employeedata/employeedata.component.ts
+++ src/app/employeedata/employeedata.component.ts
@@ -7,8 +7,8 @@ import { EmployeeService } from '../employee.service';
   styleUrls: ['./employeedata.component.css']
 })
 export class EmployeedataComponent implements OnInit {
-  employees: any[] = [];  // Storing employees
-  newEmployee = {         // Storing form input data
+  employees: any[] = [];  
+  newEmployee = {    
     empId: 0,
     empName: '',
     empEmail: ''
@@ -20,18 +20,16 @@ export class EmployeedataComponent implements OnInit {
     this.getEmployees();
   }
 
-  // Fetch all employees from backend
   getEmployees(): void {
     this.employeeService.getEmployees().subscribe((data) => {
       this.employees = data;
     });
   }
 
-  // Add new employee
   addEmployee(): void {
     this.employeeService.createEmployee(this.newEmployee).subscribe(() => {
-      this.getEmployees(); // Refresh the employee list after adding
-      this.newEmployee = { empId: 0, empName: '', empEmail: '' };  // Reset form
+      this.getEmployees(); 
+      this.newEmployee = { empId: 0, empName: '', empEmail: '' };  
     });
   }
 }
