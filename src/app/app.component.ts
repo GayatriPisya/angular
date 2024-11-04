15923a3bf576907eb2e8cc3b7c5a8965e3642540
Gayatri Pisya <gayatripisya@gmail.com>
2024-11-04 15:13:02 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ParentComponent } from './parent/parent.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
