b8f822fa51804bc860a0fe58fbe22f4f740a9594
Gayatri Pisya <gayatripisya@gmail.com>
2024-11-05 15:21:06 +0530

    updated

--- src/app/employ.service.ts
+++ src/app/employ.service.ts
@@ -9,7 +9,7 @@ export class EmployService {
     return [
       { id: 1, firstname: 'Tulugu', lastname: 'Babu', email: 'tulugukishorebabu@gmail.com' },
       { id: 2, firstname: 'Modi', lastname: 'Sireesha', email: 'modisireesha09@gmail.com' },
-      { id :3, firstname: 'Pisya',lastname: 'Gayatri', email: 'gayatripisya@gmail.com'}
+      { id :3, firstname: 'Pisya',lastname: 'Gayatri', email: 'gayatripisya@gmail.com'},
     ];
   }
 }
